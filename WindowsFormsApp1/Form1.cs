145d1a91f8ceda31e5150dbe4c472b4ea6eb32a4
Madara Uchiha <izidyzman@gmail.com>
2022-04-17 19:21:57 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int index = listBox1.SelectedIndex; 
+            string str = (string)listBox1.Items[index];
+            char[] chars = str.ToCharArray();
+            for(int i = 0; i<chars.Length;i++)
+            {
+                if (char.ToUpper(chars[i]) >= 'A' && char.ToUpper(chars[i]) <= 'Z')
+                    chars[i] = '+';
+            }
+            string str1 = new string(chars);
+            label1.Text= str1;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBox1.SelectedIndex = 0;
+        }
+    }
+}
